062d3115113232e91daf90813cf2fd4c9fffa6da
Anaïs Ferrera <anais.ferrera99@gmail.com>
2021-01-28 20:12:34 +0100

    'hola'

--- controllers/controllerUser.js
+++ controllers/controllerUser.js
@@ -1,62 +1,3 @@
-const jwt = require('jsonwebtoken');
-function createToken(user) {
-    return jwt.sign({id: user.id, username: user.username}, "My so secret sentence");
-}
-
-function signin(req, res) {
-
-    let User = require('../Models/modelUser');
-
-	User.findOne({email: req.body.account}, function(err, user) {
-		if (err)
-			throw err;
-
-		if (user.comparePassword(req.body.password)) {
-            req.session.email = req.body.account;
-			req.session.logged = true;
-			res.status(200).json({token: createToken(user)});
-		}
-		else
-			res.redirect('/');
-	});
-}
-
-function signup(req, res) {
-
-    let User = require('../Models/modelUser');
-	let user = new User();
-
-	user.email = req.body.account;
-	user.password = req.body.password;
-
-	user.save((err, savedUser) => {
-
-		if (err)
-			throw err;
-
-		res.redirect('/');
-
-	});
-}
-
-function signout(req, res) {
-
-    req.session.email = "";
-	req.session.logged = false;
-    res.redirect("/");
-
-}
-
-function profile(req, res) {
-
-    if (req.session.logged)
-        res.send("Profile");
-    else
-        res.redirect('/');
-
-}
-
-
 function createUser(req, res) {
     let User = require('../Models/modelUser');
     let newUser =  User ({
@@ -64,7 +5,6 @@ function createUser(req, res) {
         firstname : req.body.firstname,
         email : req.body.email,
         password:req.body.password,
-        token: generateToken(newUser)
     });
   
     newUser.save()
@@ -134,7 +74,3 @@ module.exports.reads = readUsers;
 module.exports.create = createUser;
 module.exports.update = updateUser;
 module.exports.delete = deleteUser;
-module.exports.signin = signin;
-module.exports.signup = signup;
-module.exports.signout = signout;
-module.exports.profile = profile;
--- routes/routeUser.js
+++ routes/routeUser.js
@@ -3,7 +3,10 @@ const router = require('express').Router();
 
 //Access the controllers
 const controller = require('../controllers/controllerUser');
-
+const User = require('../Models/modelUser.js');
+const JWT_SECRET = require('../secrets/secret');
+const Jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 //CREATE
 router.post("/user", (req, res) => {
@@ -34,5 +37,77 @@ router.delete("/user/:id", (req, res) => {
     
     controller.delete(req, res);
 });
+
+router.get('/createUser', async (req, res) => {
+    // create a user a new user
+    var testUser = new User({
+        name: 'dupont',
+        firstname: 'jean',
+        email: 'jeandupont@gmail.com',
+        password: 'Motdepasse123'
+    });
+    try {
+        // save user to database
+        await testUser.save() 
+        res.send('OK utilisateur')
+      
+    } catch (error) {
+      res.send('Utilisateur existe deja')
+      console.error(error);
+    }
+})
+
+router.get('/testMdp', (req, res) => {
+    // fetch user and test password verification
+    User.findOne({ email: 'jeandupont@gmail.com' }, function (err, user) {
+        if (err) throw err;
+
+        // test a matching password
+        user.comparePassword('Motdepasse123', function (err, isMatch) {
+            if (err) throw err;
+            console.log('Motdepasse123:', isMatch); // -> Motdepasse123: true
+        });
+
+        // test a failing password
+        user.comparePassword('123Motdepasse', function (err, isMatch) {
+            if (err) throw err;
+            console.log('123Motdepasse:', isMatch); // -> 123Motdepasse: false
+        });
+    });
+
+    res.send('OKMdp')
+})
+
+router.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) throw err;
+  
+        // test a matching password
+        user.comparePassword(req.body.password, function (err, isMatch) {
+            if (err) throw err;
+        
+            if(req.body.password , isMatch){
+                
+                const token = Jwt.sign({id: user._id, password: user.password, email: user.email},
+                '123456',
+                {expiresIn:'1 week'})
+
+                res.send(token)
+            } else {
+                res.send("mot de passe ou email incorrect")
+            }
+        });
+
+
+    });
+});
+router.get('/', passport.authenticate('jwt'), function (req, res){
+    res.json(req.user)
+});
+
+  router.get('*', function(req, res){
+    res.send('ERREUR 404', 404); 
+  });
+
 
 module.exports = router;
